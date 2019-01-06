240527a665ba766b76f75b64c9dd92d64a81c90c
Paulo Lima <paulobruny@gmail.com>
2019-01-05 23:16:51 -0300

    testing

---
+++ build/precache-manifest.8b35ef27f07008a6b77de5a241d901a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/build/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3923d73d01a4271659cc",
+    "url": "/build/static/js/runtime~main.3923d73d.js"
+  },
+  {
+    "revision": "7855433906a5c59e1545",
+    "url": "/build/static/js/main.78554339.chunk.js"
+  },
+  {
+    "revision": "3ada8b969cc63fde694e",
+    "url": "/build/static/js/1.3ada8b96.chunk.js"
+  },
+  {
+    "revision": "7855433906a5c59e1545",
+    "url": "/build/static/css/main.2b36077e.chunk.css"
+  },
+  {
+    "revision": "3ada8b969cc63fde694e",
+    "url": "/build/static/css/1.1c48d0d4.chunk.css"
+  },
+  {
+    "revision": "43dfee9d27dee50f332bf7819adc32e7",
+    "url": "/build/index.html"
+  }
+];
