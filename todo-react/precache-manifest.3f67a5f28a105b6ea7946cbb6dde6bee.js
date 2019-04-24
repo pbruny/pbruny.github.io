2b5ff447b5a2b45f888ea656ed2ee3febba3d3ac
Paulo Lima <paulobruny@gmail.com>
2019-04-24 09:56:21 -0300

    Add todo app using react

---
+++ todo-react/precache-manifest.3f67a5f28a105b6ea7946cbb6dde6bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1bfcafd8dafe8b87ec72",
+    "url": "/static/js/main.a5691a29.chunk.js"
+  },
+  {
+    "revision": "febd164012a168331732",
+    "url": "/static/js/2.ae3ff984.chunk.js"
+  },
+  {
+    "revision": "1bfcafd8dafe8b87ec72",
+    "url": "/static/css/main.5cda02fa.chunk.css"
+  },
+  {
+    "revision": "e08a3d67abcc32be478749b7840d1b58",
+    "url": "/index.html"
+  }
+];
