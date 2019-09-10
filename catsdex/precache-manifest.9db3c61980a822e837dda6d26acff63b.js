fd4a6e29cc1e3ddc0901141949f839cc86917da0
Paulo Bruny Lima <paulobruny@gmail.com>
2019-09-10 10:08:10 -0300

    add catsdex project

---
+++ catsdex/precache-manifest.9db3c61980a822e837dda6d26acff63b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd49b6cdd5144c71b396009880ddebd",
+    "url": "/catsdex/index.html"
+  },
+  {
+    "revision": "dc82a9d5cbd298be9fec",
+    "url": "/catsdex/static/css/main.d5f94795.chunk.css"
+  },
+  {
+    "revision": "46a1d5268c35a55cf7ed",
+    "url": "/catsdex/static/js/2.45c9a6bc.chunk.js"
+  },
+  {
+    "revision": "dc82a9d5cbd298be9fec",
+    "url": "/catsdex/static/js/main.95466eee.chunk.js"
+  },
+  {
+    "revision": "a1b8ff053f5c832698a0",
+    "url": "/catsdex/static/js/runtime~main.acec744d.js"
+  }
+]);
