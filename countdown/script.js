72fd8fa0717c78d5ac11a2543dddf5323c7257c5
Paulo Bruny Lima <paulobruny@gmail.com>
2020-04-09 22:54:49 -0300

    change timer

--- countdown/script.js
+++ countdown/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("April 10, 2020 15:00:00").getTime();
+var countDownDate = new Date("April 10, 2020 00:05:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
